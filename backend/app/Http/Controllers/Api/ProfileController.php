6965fe656100c4c446cda737df6841fe698002fd
al1ness <fightviol@gmail.com>
2025-09-01 17:03:40 +0300

    Create base CRUD methods

--- backend/app/Http/Controllers/Api/ProfileController.php
+++ backend/app/Http/Controllers/Api/ProfileController.php
@@ -1,34 +1,51 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Profile\StoreRequest;
+use App\Http\Requests\Api\Profile\UpdateRequest;
+use App\Http\Resources\Profile\ProfileResource;
+use App\Models\Profile;
+use App\Services\ProfileService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ProfileController extends Controller
 {
-    public function index()
+    public function index(): array
     {
-        //
+        return ProfileResource::collection(Profile::all())->resolve();
     }
 
-    public function store(Request $request)
+    public function store(StoreRequest $request): array
     {
-        //
+        $data = $request->validated();
+        $profile = Profile::create($data);
+        return ProfileResource::make($profile)->resolve();
     }
 
-    public function show(string $id)
+    public function show(Profile $profile): array
     {
-        //
+        return ProfileResource::make($profile)->resolve();
     }
 
-    public function update(Request $request, string $id)
+    public function update(Profile $profile, UpdateRequest $request): array
     {
-        //
+        $data = $request->validated();
+        $profile = ProfileService::update($profile, $data);
+        return ProfileResource::make($profile)->resolve();
     }
 
-    public function destroy(string $id)
+    public function destroy(Profile $profile): JsonResponse
     {
-        //
+        $profile->delete();
+
+        return response()->json([
+            'message' => 'Profile deleted'
+        ], Response::HTTP_OK);
     }
 }
