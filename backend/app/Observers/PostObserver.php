85aaf04c8761620c37516872a448fb56bda52c6f
al1ness <fightviol@gmail.com>
2025-09-30 23:23:32 +0300

    Create PostObserver

---
+++ backend/app/Observers/PostObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Log;
+use App\Models\Post;
+
+class PostObserver
+{
+    /**
+     * Handle the Post "created" event.
+     */
+    public function created(Post $post): void
+    {
+        Log::create([
+            'model_name' => Post::class,
+            'event_name' => "Create post id-{$post->id}",
+            'new_fields' => json_encode($post->getDirty(), JSON_UNESCAPED_UNICODE)
+        ]);
+    }
+
+    /**
+     * Handle the Post "updated" event.
+     */
+    public function updated(Post $post): void
+    {
+        $dirtyFields = $post->getDirty();
+        $oldValues = [];
+
+        foreach ($dirtyFields as $field => $newValue) {
+            $oldValues[$field] = $post->getOriginal($field);
+        }
+
+        Log::create([
+            'model_name' => Post::class,
+            'event_name' => "Updated post id-{$post->id}",
+            'old_fields' => json_encode($oldValues, JSON_UNESCAPED_UNICODE),
+            'new_fields' => json_encode($dirtyFields, JSON_UNESCAPED_UNICODE)
+        ]);
+    }
+
+    /**
+     * Handle the Post "deleted" event.
+     */
+    public function deleted(Post $post): void
+    {
+        Log::create([
+            'model_name' => Post::class,
+            'event_name' => "Delete post id-{$post->id}"
+        ]);
+    }
+
+    /**
+     * Handle the Post "retrieved" event.
+     */
+    public function retrieved(Post $post): void
+    {
+        Log::create([
+            'model_name' => Post::class,
+            'event_name' => "Retrieved post id-{$post->id}"
+        ]);
+    }
+}
