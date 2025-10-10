3afca0867446d0cac6aefcfc6fad8b0c6fb296c8
al1ness <fightviol@gmail.com>
2025-10-11 01:35:18 +0300

    Add api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,30 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TagController;
+use App\Http\Middleware\CheckPermissionMiddleware;
+use App\Http\Middleware\IsAdminMiddleware;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ChatController;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\FollowController;
 use App\Http\Controllers\Api\CommentController;
 use App\Http\Controllers\Api\CategoryController;
+
+
+Route::post('auth/login', [AuthController::class, 'login']);
+Route::group(['middleware' => 'jwt.auth', 'prefix' => 'auth'], function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
+
+Route::group(['middleware' => ['jwt.auth', CheckPermissionMiddleware::class]], function () {
+    Route::apiResource('posts', PostController::class);
+    Route::apiResource('chats', ChatController::class);
+    Route::apiResource('comments', CommentController::class);
+});
+
 
 Route::prefix('posts')->group(function () {
     Route::get('', [PostController::class, 'index']);
@@ -16,9 +34,6 @@ Route::prefix('posts')->group(function () {
     Route::delete('/{post}', [PostController::class, 'destroy']);
 });
 
-Route::resource('tags', TagController::class);
-Route::resource('chats', ChatController::class);
-Route::resource('follows', FollowController::class);
-Route::resource('comments', CommentController::class);
-Route::resource('categories', CategoryController::class);
+
+
 
