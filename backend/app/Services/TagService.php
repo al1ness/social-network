09e35aa293cef4a08d8a25bdfa05faeff2b8eeb7
al1ness <fightviol@gmail.com>
2025-11-16 12:25:05 +0300

    Create storeBatch method for save tags

--- backend/app/Services/TagService.php
+++ backend/app/Services/TagService.php
@@ -8,17 +8,29 @@ use App\Models\Tag;
 
 class TagService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
     public static function update(Tag $tag, array $data): Tag
     {
         $tag->update($data);
         return $tag->refresh();
+    }
+
+    public static function storeBatch(string $tags): array
+    {
+        if (empty($tags)) return [];
+
+        $tagsArr = explode(',', $tags);
+        $tags = [];
+
+        foreach ($tagsArr as $tagTitle) {
+            $tagTitle = trim($tagTitle);
+
+            if (!empty($tagTitle)) {
+                $tags[] = Tag::firstOrCreate([
+                    'title' => $tagTitle
+                ]);
+            }
+        }
+
+        return $tags;
     }
 }
