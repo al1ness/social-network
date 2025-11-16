4a03e6ff7bc33eb3b1804aefa9786523661bf79e
al1ness <fightviol@gmail.com>
2025-11-16 12:24:30 +0300

    Create store method

--- backend/app/Services/PostService.php
+++ backend/app/Services/PostService.php
@@ -4,21 +4,77 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Exceptions\Post\PostCreationException;
+use App\Exceptions\Post\PostUpdateException;
 use App\Models\Post;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class PostService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public static function store(array $data): Post
     {
-        //
+        try {
+            DB::beginTransaction();
+            $post = Post::create($data['post']);
+
+            if (isset($data['images'])) {
+                $uploadedPaths = ImageService::storeBatch($post, $data['images']);
+            }
+
+            if (isset($data['tags'])) {
+                $tags = TagService::storeBatch($data['tags']);
+                $post->tags()->attach(array_column($tags, 'id'));
+            }
+
+            DB::commit();
+
+        } catch (\Exception $exception) {
+            DB::rollBack();
+
+            if (!empty($uploadedPaths)) {
+                Storage::disk('s3')->delete($uploadedPaths);
+            }
+
+            throw new PostCreationException(
+                $post,
+                'Unexpected error during post creation: ' . $exception->getMessage()
+            );
+        }
+
+        return $post;
     }
 
     public static function update(Post $post, array $data): Post
     {
-        $post->update($data);
+        try {
+            DB::beginTransaction();
+            $post->update($data['post']);
+
+            if (isset($data['images'])) {
+                $uploadedPaths = ImageService::storeBatch($post, $data['images']);
+            }
+
+            if (isset($data['tags'])) {
+                $tags = TagService::storeBatch($data['tags']);
+                $post->tags()->sync(array_column($tags, 'id'));
+            }
+
+            DB::commit();
+
+        } catch (\Exception $exception) {
+            DB::rollBack();
+
+            if(!empty($uploadedPaths)) {
+                Storage::disk('s3')->delete($uploadedPaths);
+            }
+
+            throw new PostUpdateException(
+                $post,
+                'Unexpected error during post update: ' . $exception->getMessage()
+            );
+        }
+
         return $post->refresh();
     }
 }
