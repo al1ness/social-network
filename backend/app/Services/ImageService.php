0d4c56bbc2fe7ba5c773aa9c43b8e88adaefcf26
al1ness <fightviol@gmail.com>
2025-11-16 12:23:57 +0300

    Create storeBatch method for upload images

--- backend/app/Services/ImageService.php
+++ backend/app/Services/ImageService.php
@@ -3,6 +3,8 @@
 namespace App\Services;
 
 use App\Models\Image;
+use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
 
 class ImageService
 {
@@ -10,5 +12,27 @@ class ImageService
     {
         $image->update($data);
         return $image->refresh();
+    }
+
+    public static function storeBatch(Post $post, array $files): array
+    {
+        if (empty($files)) return [];
+
+        $uploadedPaths = [];
+
+        foreach ($files as $file) {
+            $fileName = $file->hashName();
+            $path = "posts/{$post->id}/{$fileName}";
+
+            Storage::disk('s3')->putFileAs("posts/{$post->id}", $file, $fileName);
+
+            $post->images()->create([
+                'file_path' => $path
+            ]);
+
+            $uploadedPaths[] = $path;
+        }
+
+        return $uploadedPaths;
     }
 }
